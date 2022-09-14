98323f848d2d80b996f884285b479dbe97164018
Juan Tuandali <101874698+juan-tuandali@users.noreply.github.com>
2022-09-14 11:21:28 -0400

    Update MacAddress_changer.py

--- Ethical_Hacking/1-changeMAC/MacAddress_changer.py
+++ Ethical_Hacking/1-changeMAC/MacAddress_changer.py
@@ -3,6 +3,29 @@ import subprocess
 # memanggil module optparse (untuk dapat menangkap argument yang dimasukkan user agar bisa digunakan dengan program)
 import optparse
 
+def getArguments():
+    parser = optparse.OptionParser()  # memanggil fungsi ini agar dapat menangkap command/perintah
+
+    # argument untuk input interface
+    parser.add_option(
+        "-i",  # menangkap argument/command -i (interface)
+        "--interface",  # or bisa juga meggunakan --interface untuk melihat option yang ada 
+        dest="interface",  # membuat var dest sebagai interface
+        help="Interface to change its MAC Address"
+    )
+
+    # argument untuk input MAC Address
+    parser.add_option(
+        "-m",  
+        "--mac", 
+        dest="newMAC", 
+        help="New MAC Address"
+    )  
+
+    return parser.parse_args() # untuk bisa menampilkan semua argumen yang dapat dipakai dan me-return nilai dari argument yang ada
+    
+# end function
+
 # start function (fungsi mengubah MAC Address)
 def changeMAC(interface, newMAC):
     print(f"[+] Changing MAC Address for {interface}")
@@ -13,25 +36,6 @@ def changeMAC(interface, newMAC):
     subprocess.call(["ifconfig", "eth0"])
 # end function
 
-parser = optparse.OptionParser()  # memanggil fungsi ini agar dapat menangkap command/perintah
-
-# argument untuk input interface
-parser.add_option(
-    "-i",  # menangkap argument/command -i (interface)
-    "--interface",  # or bisa juga meggunakan --interface untuk melihat option yang ada 
-    dest="interface",  # membuat var dest sebagai interface
-    help="Interface to change its MAC Address"
-)
-
-# argument untuk input MAC Address
-parser.add_option(
-    "-m",  
-    "--mac", 
-    dest="newMAC", 
-    help="New MAC Address"
-)  
-
-(options, argument) = parser.parse_args()  # untuk menangkap value dari argument yang dinput
 print("MAC ADDRESS BEFORE CHANGE")
 subprocess.call("ifconfig eth0", shell=True)
 
@@ -42,4 +46,6 @@ interface = options.interface  # interface = input("Interface: ")
 newMAC = options.newMAC  # newMAC = input("Enter the new MAC Address: ")
 print("MAC ADDRESS AFTER CHANGE")
 
-changeMAC(interface, newMAC)  # panggil function
+(options, arguments) = getArguments() # panggil function dalam variabel
+changeMAC(options.interface, options.newMAC) # panggil function
+
